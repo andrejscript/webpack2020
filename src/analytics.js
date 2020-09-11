519138c018f9d58d640973659ac41bed393ca289
AndreWeb <andzion105@gmail.com>
2020-09-11 15:51:38 +0300

    first commit

---
+++ src/analytics.js
@@ -0,0 +1,25 @@
+function createAnalytics() {
+    let counter = 0;
+    let destroyed = false;
+
+    console.log('111');
+
+    const listener = () => counter++;
+
+    document.addEventListener('click', listener);
+    return {
+        destroy() {
+            document.removeEventListener('click', listener);
+            destroyed = true;
+        },
+
+        getClicks() {
+            if(destroyed) {
+                return `Analytics is destroyed! Total clicks = ${counter}`;
+            }
+            return counter;
+        }
+    };
+}
+
+window.analytics = createAnalytics();
